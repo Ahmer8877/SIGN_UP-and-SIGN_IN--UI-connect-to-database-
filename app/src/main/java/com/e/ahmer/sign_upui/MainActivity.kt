ded8cdfc0956b8f61d305d7417cc1b5c8f0d8604
Ahmir Shafiq <rajpootahmer786@gmail.com>
2025-08-27 13:04:15 +0500

    SIgn up

---
+++ app/src/main/java/com/e/ahmer/sign_upui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.e.ahmer.sign_upui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //splash screen code
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent= Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        },3000)
+
+        }
+    }
