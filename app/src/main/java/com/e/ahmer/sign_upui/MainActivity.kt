fcc676ad08028494e64329096f173101c71a750f
Ahmir Shafiq <rajpootahmer786@gmail.com>
2025-08-29 09:30:24 +0500

    sign up commit

--- app/src/main/java/com/e/ahmer/sign_upui/MainActivity.kt
+++ app/src/main/java/com/e/ahmer/sign_upui/MainActivity.kt
@@ -3,11 +3,9 @@ package com.e.ahmer.sign_upui
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +17,8 @@ class MainActivity : AppCompatActivity() {
         Handler().postDelayed({
             val intent= Intent(this, SignUpActivity::class.java)
             startActivity(intent)
-        },3000)
+            finish()
+        },1000)
 
         }
     }
--- app/src/main/java/com/e/ahmer/sign_upui/SignUpActivity.kt
+++ app/src/main/java/com/e/ahmer/sign_upui/SignUpActivity.kt
@@ -1,10 +1,7 @@
 package com.e.ahmer.sign_upui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
